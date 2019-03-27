494b5803f784d7a49667be770d777b01e747961b
pk1257861 <2823842330@qq.com>
2019-03-27 15:43:41 +0800

    toplist

--- src/main.js
+++ src/main.js
@@ -2,10 +2,39 @@ import Vue from 'vue'
 import App from './App.vue'
 
 //全局引入样式
-import style from './assets/statics/site/css/style.css'
+import  './assets/statics/site/css/style.css'
+
+//路由
+import VueRouter from "vue-router"
+Vue.use(VueRouter)
+
+//导入组件
+import index from './components/index.vue'
+import cart from './components/cart.vue'
+
+//规则
+let routes=[
+  {
+    path:'/',
+    component:index
+},
+{
+  path:'/index',
+  component:index
+},{
+  path:'/cart',
+  component:cart
+}
+]
+
+let router =new VueRouter({
+  routes
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
